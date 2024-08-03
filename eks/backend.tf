f566c673cd5c79509b7a9d0024b405e62738f53d
cookiesikeri <ebenezer.ikeri@bankly.ng>
2024-08-04 00:44:51 +0100

    jh

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,8 +7,8 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "my-ews-baket1"
-    region         = "us-east-1"
+    bucket         = "tf-dynamo"
+    region         = "eu-west-2"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
     encrypt        = true
